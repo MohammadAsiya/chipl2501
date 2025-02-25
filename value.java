290be3e30e2cfea721544f555e661ee03dd468ca
ParvathiKollipara <parvathikollipara8106@gmail.com>
2025-02-25 15:16:52 +0530

    Second Commit

--- value.java
+++ value.java
@@ -3,8 +3,8 @@ class value
 	public static void main(String args[])
 	{
 		int num=124;
-		char capital='A';
-		char small='a';
+		char capital='B';
+		char small='b';
 		System.out.println(num>100?(char)(capital+32):(char)(small-32));
 	}
 }
